561849e79007a808721f698e9e2930dd1802f3a7
Richard Coney <richardconey46@gmail.com>
2026-05-19 16:20:25 -0400

    Added songs array along with data for each song

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+        name: "California Love",
+        path: "assets/music/California Love.mp3",
+        artist: "2Pac",
+        cover: "assets/images/California Love Cover.jpg"
+    },
+    {
+        name: "Espresso",
+        path: "assets/music/Espresso.mp3",
+        artist: "Sabrina Carpenter",
+        cover: "assets/images/Espresso Cover.jpg"
+    },
+    {
+        name: "Just A Friend",
+        path: "assets/music/Just A Friend.mp3",
+        artist: "Biz Markie",
+        cover: "assets/images/Just A Friend Cover.jpg"
+    },
+    {
+        name: "Walking On A Thin Line",
+        path: "assets/music/Walking On A Thin Line.mp3",
+        artist: "Huey Lewis and the News",
+        cover: "assets/images/Walking On A Thin Line Cover.jpg"
+    },
+    {
+        name: "Thriller",
+        path: "assets/music/Thriller.mp3",
+        artist: "Michael Jackson",
+        cover: "assets/images/Thriller Cover.jpg"
+    },
+    {
+        name: "Big Poppa",
+        path: "assets/music/Big Poppa.mp3",
+        artist: "The Notorious B.I.G.",
+        cover: "assets/images/Big Poppa Cover.jpg"
+    },
+    {
+        name: "In The Air Tonight",
+        path: "assets/music/In The Air Tonight.mp3",
+        artist: "Phil Collins",
+        cover: "assets/images/In The Air Tonight Cover.jpg"
+    },
+    {
+        name: "All Inclusive",
+        path: "assets/music/All Inclusive.mp3",
+        artist: "Pool Boy & Cyril Hahn",
+        cover: "assets/images/All Inclusive Cover.jpg"
+    },
+    {
+        name: "Fight For Us",
+        path: "assets/music/Fight For Us.mp3",
+        artist: "Kalax",
+        cover: "assets/images/Fight For Us Cover.jpg"
+    },
+    {
+        name: "Girl Forever",
+        path: "assets/music/Girl Forever.mp3",
+        artist: "Satin Jackets",
+        cover: "assets/images/Girl Forever Cover.jpg"
+    }
+]
